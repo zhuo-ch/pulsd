71f47f45754c6fa588074a2cedd85a24e37d37f8
Zhuo Chen <zhuo.h.chen@gmail.com>
2018-01-31 13:31:36 -0500

    Add dependency react-dom.

---
+++ lib/components/event_list.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class EventList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1></h1>
+    );
+  }
+}
+
+export default EventList;
