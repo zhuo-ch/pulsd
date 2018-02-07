22ac0c957484d55b175f38afcca75ce65862f0fc
Zhuo Chen <zhuo.h.chen@gmail.com>
2018-02-07 13:39:13 -0500

    Fix eventbrite object.

--- functions/index.js
+++ functions/index.js
@@ -14,14 +14,15 @@ exports.syndicate = functions.database
   .ref('/events/{eventId}')
   .onCreate(event => {
     const callback = (data, functions) => {
-      console.log(functions.database.ref('/submissions'));
-      console.log(data);
       return event.data.ref.child('submissions').set(data);
     }
+    const formData = event.data.val();
 
-    return rp(sendEventbrite(event))
+    rp.post(sendEventbrite(formatEventbrite(formData)))
       .then(data => callback(data, functions))
       .catch(err => console.log(err.error));
+
+    rp.post(sendXing)
   });
 
 const sendXing = event => {
@@ -33,21 +34,28 @@ const sendXing = event => {
     hostId: keys.xingId
   }
 
-  const options = {
+  return ({
     url: 'https://www.xing-events.com/api/event/create',
     method: 'POST',
     data: data
-  }
-
-  return options;
+  });
 }
 
 const sendEventbrite = event => {
   return ({
     url: 'https://www.eventbriteapi.com/v3/events/',
-    method: 'POST',
     headers: { "Authorization": `Bearer ${keys.eventbrite}` },
-    data: event,
-    json: true
+    data: { event },
+  });
+}
+
+const formatEventbrite = event => {
+  return JSON.stringify({
+    name: { html: event.name },
+    description: event.description,
+    start: { timezone: event.start_time_zone, utc: event.start },
+    end: { timezone: event.end_time_zone, utc: event.end },
+    currency: event.currency,
+    listed: false,
   });
 }
